859d87244b2988ca185a306ccdbecbb046a2cbf0
Felipe Alves <felipe.rod.alves@gmail.com>
2019-06-11 11:49:59 -0300

    Class 01 - Node Express Backend Server
    creating app for backend with node.js
    - create posts
    - list posts
    - like posts
    - http and websocket configuration

---
+++ backend/src/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const cors = require("cors");
+
+const app = express();
+
+/* server split for http and websocket request */
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+
+/* DB communication */
+mongoose.connect("mongodb://localhost:27017/instaRocket", {
+  useNewUrlParser: true
+});
+
+/* allow all Controller to use websocket */
+app.use((req, res, next) => {
+  req.io = io;
+  next();
+});
+
+/* allow other applications to access the application */
+app.use(cors());
+app.use("/files", express.static(path.resolve(__dirname, "..", "resized")));
+app.use(require("./routes"));
+
+server.listen(3333);
