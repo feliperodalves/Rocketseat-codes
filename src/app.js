f1513e4f1f33396784b68ea2ec9617052ebd2581
Felipe Alves <felipe.rod.alves@gmail.com>
2019-06-27 14:03:46 -0300

    New Features for backend
    - Sequelize configuration with postgres (using docker)
    - MVC folder structure
    - ESLint, Prettier and EditorConfig implementation
    - User and Session Models/Controllers
    - Hash with bcrypt and JWT authentication
    - Authentication throw middleware
    - Yup for data validation

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
-import express from "express";
-import routes from "./routes";
+import express from 'express';
+import routes from './routes';
+
+import './database';
 
 class App {
   constructor() {
@@ -12,6 +14,7 @@ class App {
   middlewares() {
     this.server.use(express.json());
   }
+
   routes() {
     this.server.use(routes);
   }
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'ead671448d2e6ea739d4f1e46aa21865',
+  expiresIn: '7d',
+};
