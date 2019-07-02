1225e93cf5d1e02f094e61596740009f5564cea8
Felipe Alves <felipe.rod.alves@gmail.com>
2019-07-02 09:46:10 -0300

    fix: Add get route to server.js

--- server.js
+++ server.js
@@ -11,8 +11,18 @@ mongoose.connect(
     'mongodb://localhost:27017/nodeapi',
     { useNewUrlParser: true }
 );
-requireDir('./src/models');
 
+requireDir('./src/models');
+const Product = mongoose.model("Product")
+
+app.get("/", (req, res) => {
+    Product.create({
+        title: 'daadsdasads',
+        description: 'adddas',
+        url: 'assasasa'
+    })
+    return res.send("Hello")
+})
 app.use('/api', require('./src/routes'));
 
 app.listen(3001);
