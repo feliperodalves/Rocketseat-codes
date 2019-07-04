784696fe89c65d80f5f437314729fe2f5ed9fbe6
Felipe Alves <felipe.rod.alves@gmail.com>
2019-07-04 16:07:53 -0300

    chore: Final commit for 'desafio 2'   - Add routes   - Add Controllers (User + Session) with data validation with yup   - Add Models (User)   - Add implementation of bcrypt for passwords   - Add implementation of JWT for session token   - Add middleware to ensure logged user

---
+++ src/app/middlewares/auth.js
@@ -0,0 +1,20 @@
+import jwt from 'jsonwebtoken';
+import { promissify } from 'util';
+import authConfig from '../../config/auth';
+
+export default async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ error: 'Token not provided' });
+  }
+  const [, token] = authHeader.slipt(' ');
+  try {
+    const decoded = await promissify(jwt.verify)(token, authConfig.secret);
+    req.userId = decoded.id;
+
+    return next();
+  } catch (e) {
+    return res.status(401).json({ error: 'Invalid Token' });
+  }
+};
