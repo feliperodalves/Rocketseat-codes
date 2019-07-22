9218b8a65dc4384dea53b89af2f299aabf2ccd01
Felipe Alves <felipe.rod.alves@gmail.com>
2019-07-22 10:03:46 -0300

    feat: Start of manual React project   - Add Webpack, Babel, Loaders(babel, css, style, file)   - Configure Default tree of elements   - Add simple default test App component

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./App.css";
+
+import background from "./assets/background.jpg";
+
+class componentName extends Component {
+  render() {
+    return <img width="100%" src={background} />;
+  }
+}
+
+export default componentName;
