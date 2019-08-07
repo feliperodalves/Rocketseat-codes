c200a29621c11e605697b06c59519649d6da0f63
Felipe Alves <felipe.rod.alves@gmail.com>
2019-08-07 11:03:44 -0300

    feat: Create frontend pages   - Create react-app   - Add assets (images)   - Add Routes configuration   - Add Global Styles with styled components   - Add Login page with styled components   - Add Main page with styled components

---
+++ frontend/src/pages/Login/styles.js
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+
+export const LoginContainer = styled.div`
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  form {
+    width: 100%;
+    max-width: 300px;
+    display: flex;
+    flex-direction: column;
+
+    input {
+      margin-top: 20px;
+      border: 1px solid #ddd;
+      border-radius: 4px;
+      height: 48px;
+      padding: 0 20px;
+      font-size: 16px;
+
+      ::placeholder {
+        color: #999;
+      }
+    }
+
+    button {
+      margin-top: 10px;
+      border: 0;
+      border-radius: 4px;
+      height: 48px;
+      font-size: 16px;
+      background: #df4723;
+      color: #fff;
+      font-weight: bold;
+    }
+  }
+`;
---
+++ frontend/src/pages/Main/index.js
@@ -0,0 +1,75 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+import api from "../../services/api";
+
+import logo from "../../assets/logo.svg";
+import dislike from "../../assets/dislike.svg";
+import like from "../../assets/like.svg";
+
+import { MainContainer, Buttons } from "./styles";
+
+export default function Main({ match }) {
+  const { id: userId } = match.params;
+
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    async function loadUsers() {
+      const response = await api.get("/devs", {
+        headers: { user: userId }
+      });
+      setUsers(response.data);
+    }
+
+    loadUsers();
+  }, [userId]);
+
+  async function handleLike(id) {
+    await api.post(`/devs/${id}/likes`, null, {
+      headers: { user: userId }
+    });
+
+    setUsers(users.filter(user => user._id !== id));
+  }
+
+  async function handleDislike(id) {
+    await api.post(`/devs/${id}/dislikes`, null, {
+      headers: { user: userId }
+    });
+
+    setUsers(users.filter(user => user._id !== id));
+  }
+
+  return (
+    <MainContainer>
+      <Link to="/">
+        <img src={logo} alt="" />
+      </Link>
+
+      {users.length > 0 ? (
+        <ul>
+          {users.map(user => (
+            <li key={user._id}>
+              <img src={user.avatar} alt="" />
+              <footer>
+                <strong>{user.name}</strong>
+                <p>{user.bio}</p>
+              </footer>
+              <Buttons>
+                <button type="button" onClick={() => handleDislike(user._id)}>
+                  <img src={dislike} alt="" />
+                </button>
+                <button type="button" onClick={() => handleLike(user._id)}>
+                  <img src={like} alt="" />
+                </button>
+              </Buttons>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <div className="empty">Acabou :(</div>
+      )}
+    </MainContainer>
+  );
+}
