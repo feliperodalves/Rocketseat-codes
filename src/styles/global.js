c670e7e15d676d6364fe853cc7614a29ff47bf54
Felipe Alves <felipe.rod.alves@gmail.com>
2019-08-07 23:59:29 -0300

    feat: Initial Commit
     - basic structure with create react-app
     - eslint & prettier configuration
     - assets
     - styles
     - routes

---
+++ src/styles/global.js
@@ -0,0 +1,31 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Roboto&display=swap');
+
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  html,body,#root{
+    min-height: 100%;
+  }
+
+  body {
+    background: #7159c1;
+    -webkit-font-smoothing: antialiased !important;
+  }
+
+  body, input, button {
+    color: #222;
+    font-size: 14px;
+    font-family: Roboto, sans-serif;
+  }
+
+  button{
+    cursor: pointer;
+  }
+`;
