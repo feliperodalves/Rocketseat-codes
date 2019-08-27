50b277be01a607e28c05e721abdcaf44fdcfe9e8
Felipe Alves <felipe.rod.alves@gmail.com>
2019-08-27 18:08:26 -0300

    feat: GoBarber - mobile - Signin SignUp Dashboard Profile pages
     - Add Store configuration (redux, sagas, reactotron, persistor)
     - Add Routes
     - Add Pages (SignIn, SignUp, Dashboard, Profile)
     - Add global components (Background, Button, Input)
     - Add dependencies

--- bootcamp/GoBarber/mobile/src/index.js
+++ bootcamp/GoBarber/mobile/src/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import {StatusBar} from 'react-native';
+import {PersistGate} from 'redux-persist/integration/react';
+import {Provider} from 'react-redux';
 
-import Routes from './routes';
+import './config/ReactotronConfig';
+import {store, persistor} from './store';
+import App from './App';
 
-const App = () => {
-  return <Routes />;
+const Index = () => {
+  return (
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+        <App />
+      </PersistGate>
+    </Provider>
+  );
 };
 
-export default App;
+export default Index;
