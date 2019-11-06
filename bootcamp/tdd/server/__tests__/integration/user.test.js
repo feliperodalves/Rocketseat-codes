cc5cf619c2eb4075ad2d29a1f57e8dab0d886bb8
Felipe Alves <felipe.rod.alves@gmail.com>
2019-11-05 23:43:28 -0300

    feat: initial TDD configuration server
     - Run omni from @rocketseat/omni
     - Configure JEST
     - Configure initial tests
     - Configure User Model & Controller

---
+++ bootcamp/tdd/server/__tests__/integration/user.test.js
@@ -0,0 +1,35 @@
+import request from 'supertest';
+import app from '../../src/app';
+
+describe('User', () => {
+  it('should be able to register', async () => {
+    const response = await request(app)
+      .post('/users')
+      .send({
+        name: 'Felipe Alves',
+        email: 'felipe.rod.alves@gmail.com',
+        password_hash: '123456',
+      });
+
+    expect(response.body).toHaveProperty('id');
+  });
+
+  it('should not be able to register with duplicated email', async () => {
+    await request(app)
+      .post('/users')
+      .send({
+        name: 'Felipe Alves',
+        email: 'felipe.rod.alves2@gmail.com',
+        password_hash: '123456',
+      });
+    const response = await request(app)
+      .post('/users')
+      .send({
+        name: 'Felipe Alves',
+        email: 'felipe.rod.alves2@gmail.com',
+        password_hash: '123456',
+      });
+
+    expect(response.status).toBe(400);
+  });
+});
