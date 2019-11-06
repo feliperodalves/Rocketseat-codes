bc50220cad012194c851c9fdfdf14668d3fed67d
Felipe Alves <felipe.rod.alves@gmail.com>
2019-11-06 12:03:55 -0300

    feat: final TDD user mode/controller
     - Using fatory-girl + faker to create mockup random data
     - Using bcrypt for encryption
     - Include bcrypt on User model (hook)
     - Stop logging sequelize
     - Trucate all tables when testing

--- bootcamp/tdd/server/__tests__/integration/user.test.js
+++ bootcamp/tdd/server/__tests__/integration/user.test.js
@@ -1,35 +1,43 @@
 import request from 'supertest';
+import bcrypt from 'bcryptjs';
 import app from '../../src/app';
 
+import truncate from '../util/truncate';
+import factory from '../factories';
+
 describe('User', () => {
+  beforeEach(async () => {
+    await truncate();
+  });
+
   it('should be able to register', async () => {
+    const user = await factory.attrs('User');
     const response = await request(app)
       .post('/users')
-      .send({
-        name: 'Felipe Alves',
-        email: 'felipe.rod.alves@gmail.com',
-        password_hash: '123456',
-      });
+      .send(user);
 
     expect(response.body).toHaveProperty('id');
   });
 
   it('should not be able to register with duplicated email', async () => {
+    const user = await factory.attrs('User');
     await request(app)
       .post('/users')
-      .send({
-        name: 'Felipe Alves',
-        email: 'felipe.rod.alves2@gmail.com',
-        password_hash: '123456',
-      });
+      .send(user);
     const response = await request(app)
       .post('/users')
-      .send({
-        name: 'Felipe Alves',
-        email: 'felipe.rod.alves2@gmail.com',
-        password_hash: '123456',
-      });
+      .send(user);
 
     expect(response.status).toBe(400);
+  });
+
+  it('should encrypt user password when new user created', async () => {
+    const user = await factory.create('User', {
+      password: '123456',
+    });
+
+    const compareHash = await bcrypt.compare('123456', user.password_hash);
+
+    expect(compareHash).toBe(true);
   });
 });
