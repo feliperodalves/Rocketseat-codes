2693b472779ce2616e49801b2871d3e81d4fd641
Felipe Alves <felipe.rod.alves@gmail.com>
2020-03-30 01:39:07 -0300

    fix: cors() & localhost

--- omnistack/omnistack-10/server/src/app.js
+++ omnistack/omnistack-10/server/src/app.js
@@ -5,12 +5,12 @@ const routes = require('./routes');
 
 const app = express();
 
-mongo.connect('mongodb://192.168.15.10:27017/omnistack', {
+mongo.connect('mongodb://localhost:27017/omnistack', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-app.use(cors);
+app.use(cors());
 app.use(express.json());
 
 app.use(routes);
