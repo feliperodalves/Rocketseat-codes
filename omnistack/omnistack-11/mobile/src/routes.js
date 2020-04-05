4e9b9675ac65cab6c0b6d98eb3d27aceee5b8b1f
Felipe Alves <felipe.rod.alves@gmail.com>
2020-04-05 20:49:20 -0300

    feature: Omnistack 11 - mobile - Final mobile development - Adicionado Rotas - Adicionado páginas de listagem e detalhes - Adicionado links para email e whatsapp - Adicionado estilização de todas páginas e App

--- omnistack/omnistack-11/mobile/src/routes.js
+++ omnistack/omnistack-11/mobile/src/routes.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-import Incidents from './pages/Incidents';
-import Detail from './pages/Detail';
+import Incidents from "./pages/Incidents";
+import Detail from "./pages/Detail";
 
 const AppStack = createStackNavigator();
 
 export default function Routes() {
   return (
     <NavigationContainer>
-      <AppStack.Navigator>
-        <AppStack.Screen component={Incidents}></AppStack.Screen>
-        <AppStack.Screen component={Detail}></AppStack.Screen>
+      <AppStack.Navigator screenOptions={{ headerShown: false }}>
+        <AppStack.Screen name="Incidents" component={Incidents} />
+        <AppStack.Screen name="Detail" component={Detail} />
       </AppStack.Navigator>
     </NavigationContainer>
   );
