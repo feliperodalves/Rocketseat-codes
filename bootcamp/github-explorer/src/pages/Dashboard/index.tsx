f1031a35dcfc8f4b8611a77d15d6f4abc18d4bd2
Felipe Alves <felipe.rod.alves@gmail.com>
2020-04-21 02:13:36 -0300

    feature: Styling Dashboard

--- bootcamp/github-explorer/src/pages/Dashboard/index.tsx
+++ bootcamp/github-explorer/src/pages/Dashboard/index.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
-import { Container, Title } from './styles';
+import { FiChevronRight } from 'react-icons/fi';
+import { Title, Form, Repositories } from './styles';
+
+import logoImg from '../../assets/images/logo.svg';
 
 export const Dashboard: React.FC = () => {
   return (
-    <Container>
-      <Title />
-    </Container>
+    <>
+      <img src={logoImg} alt="Github explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="">
+          <img src="" alt="" />
+          <div>
+            <strong />
+            <p />
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
   );
 };
