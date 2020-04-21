b50d6b03a7db9f8d9dff30b2c9bd9f98037887f4
Felipe Alves <felipe.rod.alves@gmail.com>
2020-04-21 01:37:59 -0300

    feature: Creating Github Explorer - Criando projeto com create react-app - Incluindo eslint/prettier/editorconfig - Adicionando router - Adicionando styled-components

---
+++ bootcamp/github-explorer/src/pages/Dashboard/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container, Title } from './styles';
+
+export const Dashboard: React.FC = () => {
+  return (
+    <Container>
+      <Title />
+    </Container>
+  );
+};
