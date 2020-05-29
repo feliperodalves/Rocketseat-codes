7a1a08e0248318ac344ad3c0ca4c5b1bfbf01c81
Felipe Alves <felipe.rod.alves@gmail.com>
2020-05-29 12:12:39 -0300

    feature: SAAS frontend - Initial Commit Configuration

---
+++ bootcamp/saas/frontend/src/styles/global.js
@@ -0,0 +1,45 @@
+import { createGlobalStyle } from 'styled-components';
+
+// import 'react-perfect-scrollbar/dist/css/styles.css';
+// import 'react-toastify/dist/ReactToastify.css';
+
+export default createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700&display=flow');
+
+  * {
+    margin:0;
+    padding:0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  *:focus {
+    outline: 0;
+  }
+
+  html, body, #root {
+    height: 100vh;
+  }
+
+  body {
+    background: #353940;
+    -webkit-font-smoothing: antialiased !important;
+  }
+
+  body, input, button {
+    font-family: 'Source Sans Pro', sans-serif;
+    text-rendering: optimizeLegibility !important;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  ul {
+    list-style: none;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
